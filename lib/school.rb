79f5b52147de065ab72170ef98810520aeaf3774
Elle Faraday <faraday.elle@gmail.com>
2017-11-09 02:54:33 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,11 @@ class School
   attr_reader :roster
 
   def add_student(name, grade)
-    @roster[grade] = []
-    @roster[grade] << name
+    if @roster.key?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+    end
   end
 end
