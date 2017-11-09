aa7287a46d078c4b56f9d12f577887e490b49b31
Elle Faraday <faraday.elle@gmail.com>
2017-11-09 02:40:38 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,11 +1,14 @@
 # code here!
 class School
 
-  def initialize
+  def initialize(name)
+    @school = name
     @roster = {}
   end
 
   attr_reader :roster
 
-
+  def add_student(name, grade)
+    @roster << grade => name
+  end
 end
